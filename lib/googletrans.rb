d7d1a325173448a27fa3a188dc4bfd7c4fe077cf
Nicoer <saber00sword@gmail.com>
2019-01-26 03:43:19 +0800

    0.2

--- lib/googletrans.rb
+++ lib/googletrans.rb
@@ -4,5 +4,13 @@ require "googletrans/gtoken"
 
 module Googletrans
   class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    def translator query,dest,src="auto",params={}
+      build(params).translator query,src,dest
+    end
+    private
+    def build params={}
+      Googletrans::Translator.new params
+    end
+  end
 end
